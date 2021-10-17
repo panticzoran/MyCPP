a5743b28f08d01dc4c5f9124b87190c3377a5598
Zoran Pantić <91920971+panticzoran@users.noreply.github.com>
2021-10-17 18:15:51 +0000

    Getting ready for exercises with the 2nd chapter

--- main.cpp
+++ main.cpp
@@ -6,19 +6,6 @@ int main() {
 
 	string name,surname;
 
-	cout << "Please enter your name: ";
-	cin >> name;
 
-	cout << "\nPlease enter your surname: ";
-	cin >> surname;
-
-	cout << '\n'
-			<< "Hello, "
-			<< name
-			<< ", with your surname "
-			<< surname
-			<< '\n';
-
-return 0;
 
 }
